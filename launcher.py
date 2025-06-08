c1bf0c7254fbf08ed1fdbef026bbc6eea137d362
yakovexplorer <yakov.zhuravlevich@mail.ru>
2025-06-08 13:54:18 +0300

    and also don't forget to change the internal version for the updater 👍

--- launcher.py
+++ launcher.py
@@ -44,7 +44,7 @@ print("-------------------------------------------------------------------------
 SSL_PATH = "./Certificate/roblox.crt"
 CLIENT_ID = '1348257279983616141'
 
-currentVer = "0_781B"
+currentVer = "0_784B"
 
 open('./launcher.log', 'w').close()
 log_file = open('./launcher.log', 'a')
